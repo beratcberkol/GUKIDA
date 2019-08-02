1bbaa5af2396e6b33bc5c3bda956eeab6bedd125
Berat Can Berkol <53617832+beratcberkol@users.noreply.github.com>
2019-08-02 17:21:23 +0200

    Add files via upload

---
+++ gukida/js/yenikayit.php
@@ -0,0 +1,68 @@
+<?php include "header2.php" ?>
+<html>
+
+<head>
+<meta name=GENERATOR content=Microsoft FrontPage 5.0>
+<meta name=ProgId content=FrontPage.Editor.Document>
+<meta http-equiv=Content-Type content=text/html; charset=windows-1254>
+<meta http-equiv=Content-Language content=tr>
+<title>Yeni Sayfa 2</title>
+</head>
+
+<body>
+
+<p>
+<!--webbot bot=PurpleText preview=NOT - Güvenlik nedeniyle bazı web sunucuları kendi kendini kaydettirmeye izin vermez. Bu sayfayı Web''e kaydettiğinizde, FrontPage bu yeteneği sınar ve bir sorun varsa, sorun oluşturan dosyayı Rapor Görünümü''nde Bileşen Hataları özetinde görüntüler. Web izinlerini kurmak için Windows 2000 veya Windows NT kullanan Microsoft web sunucularında da, kendi kendini kaydettirmeye izin verilmez. --></p>
+<p>
+<!--webbot bot=PurpleText preview=Kullanıcı Kayıt Formu -  Bu sayfa gerektiği gibi yapılandırılıp web sunucusundaki Kök Web''ine kaydedildiğinde, bu form kullanıcıların kendilerini, aynı web sunucusundaki geçerli FrontPage alt web kullanıcıları olarak kaydettirmelerine izin verir. Kullanıcılar bu formu gönderdiğinde hedef alt web var olmalıdır. Bu sayfayı yapılandırmak için, önce hedef alt web''in adını ayarlamak üzere form özelliklerini düzenleyin. Sonra da bu sayfadaki metnin [Alt web''in adı] tüm örneklerini hedef alt web''in başlığıyla (örneğin, "Yeni Fikirler Web" veya "Çalışanlar Tartışma Web''i" gibi) değiştirmek için bu özelliği kullanın. Bu sayfayı Kök Web''e kaydedin. Ardından, Araçlar menüsündeki Sunucu komutunu kullanarak hedef alt web''in izinlerini değiştirin. İzinleri hedef alt web için benzersiz hale getirerek, Kullanıcı erişim denetimlerini salt kayıtlı kullanıcıların girebileceği şekilde değiştirin. --></p>
+<h1>[Alt web''inizin adı] Kullanıcı Kaydı</h1>
+<hr>
+<p>Bu formu doldurup göndererek kendinizi otomatik olarak [Alt web''inizin adı] 
+kullanıcısı olarak kaydettirebilirsiniz. [Alt web''inizin adı] alt web''ine 
+yalnızca kayıtlı kullanıcıların girmesine izin verilir. Kendinize bir kullanıcı 
+adı (soyadınız olabilir) seçin; kullanıcı adındaki karakterler arasında boşluk 
+olmadığına emin olun. Ayrıca kişisel bir parola oluşturun. Bu ikisi artık, [Alt 
+web''inizin adı] için sizin "anahtarınız" olacaktır. Bu bilgi, sıradan 
+kullanıcıların giremeyeceği, yalnızca web yöneticisinin girebileceği bir kayıt 
+veritabanında saklanacaktır.</p>
+<p>[Alt web''inizin adı] gibi korumalı bir web''e sahip olmanın başlıca 
+yararlarından biri, yetkilendirilmiş kullanıcıların, tartışma grubuna makale 
+gönderme formu gibi formlarda form alanlarına adlarını yazmak zorunda 
+kalmamalarıdır; web sunucusu formu yazanın kim olduğunu zaten bilir. Benzer 
+şekilde, diğer kullanıcılar da sizin adınızla gönderilen makale ve postaların 
+gerçekten sizin tarafınızdan gönderildiğinden, sizin adınız kullanılarak başka 
+biri tarafından göndermediğinden emin olacaklardır.</p>
+<p>Başarıyla kaydolduktan sonra, [Alt web''inizin adı] alt web''ine ilk erişim 
+girişiminizde web tarayıcınız kullanıcı adınızı ve parolanızı yazmanızı 
+isteyecektir. Tarayıcı, çalışmaya devam ettiği süre boyunca bu bilgiyi anımsar, 
+bu şekilde bu bilgi yeniden istenmeden [Alt web''inizin adı] içindeki herhangi 
+bir belgeye erişebilirsiniz.</p>
+<hr>
+<form method=POST action=--WEBBOT-SELF-->
+  <!--webbot bot=Registration s-service=Alt web''inizin adı s-username-fields=Kullanıcı Adı s-password-fields=Parola Parola Onayı i-password-min-length=6 b-check-password-against-user=TRUE u-file=_private/regdb.txt s-format=TEXT/TSV s-label-fields=TRUE s-builtin-fields=Date Time REMOTE_NAME HTTP_USER_AGENT s-form-fields=Kullanıcı Adı Eposta Adresi -->
+  <h2>Form Gönderme</h2>
+  <p>Kullanıcı adı oluşturun:<br>
+  <input type=text size=25 maxlength=256 name=KullaniciAdi> -- <em>
+  BÜYÜK/küçük harf kullanabilirsiniz</em><br>
+  Parola oluşturun:<br>
+  <input type=password size=25 maxlength=256 name=Parola> -- <em>bunu 
+  gizli tutun!</em><br>
+  Parolayı yeniden girin:<strong><br>
+  </strong><input type=password size=25 maxlength=256 name=ParolaOnayi> 
+  -- <em>doğrulamak için</em><br>
+  E-posta adresi girin:<strong><br>
+  </strong><input type=text size=25 maxlength=256 name=EpostaAdresi> --
+  <em>e-posta adresiniz varsa</em></p>
+  <h2><input type=submit value=Beni Kaydet>
+  <input type=reset value=Formu Temizle></h2>
+</form>
+<hr>
+<h5>Yazar bilgileri buraya yerleştirilir.<br>
+Telif Hakkı © 2001 [Kuruluşun Adı]. Tüm hakları saklıdır.<br>
+Son düzeltme tarihi:
+<!--webbot bot=TimeStamp s-type=EDITED s-format=%d %B %Y -->.</h5>
+
+</body>
+
+</html>
+<?php include "footer.php" ?>
